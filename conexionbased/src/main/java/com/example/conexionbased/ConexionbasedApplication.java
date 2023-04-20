2ef99f72f2290617e5f99cc4a880b69d0da7a7e1
Ivan <ivancarreton@gmail.com>
2023-04-20 16:59:20 +0200

    Initial commit

---
+++ conexionbased/src/main/java/com/example/conexionbased/ConexionbasedApplication.java
@@ -0,0 +1,13 @@
+package com.example.conexionbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionbasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionbasedApplication.class, args);
+	}
+
+}
---
+++ conexionbased/src/test/java/com/example/conexionbased/ConexionbasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conexionbased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionbasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
