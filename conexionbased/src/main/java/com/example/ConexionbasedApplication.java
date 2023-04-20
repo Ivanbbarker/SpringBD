7f6afe878a8f9b97983068229aae7f7d0a30d8c2
Ivan <ivancarreton@gmail.com>
2023-04-20 20:08:42 +0200

    Final Generacion de Tabla SQL
    Al final se genera la tabla SQL, dentro de nuestra base de datos, al correr el archivo que genera el archivo create.sql

--- conexionbased/src/main/java/com/example/ConexionbasedApplication.java
+++ conexionbased/src/main/java/com/example/ConexionbasedApplication.java
@@ -3,15 +3,14 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+/*Para utilizar repositorios JPA, tenemos que indicarlo a Spring. Podemos hacer esto con @EnableJpaRepositories.
+Tenga en cuenta que tenemos que usar esta anotación con @Configuration: */
 @SpringBootApplication
 @EnableJpaRepositories(basePackages= "com.example.conexionbased")
 public class ConexionbasedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConexionbasedApplication.class, args);
-
-
 	}
 
 }
--- create.sql
+++ create.sql
@@ -1,4 +1,22 @@
 
+
+
+    create table bicicleta (
+        id bigint not null auto_increment,
+        cliente varchar(255),
+        marca varchar(255),
+        modelo varchar(255),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table bicicleta (
+       id bigint not null auto_increment,
+        cliente varchar(255),
+        marca varchar(255),
+        modelo integer not null,
+        primary key (id)
+    ) engine=InnoDB;
+
     create table bicicleta (
        id bigint not null auto_increment,
         cliente varchar(255),
