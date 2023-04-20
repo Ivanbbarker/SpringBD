fbb58d25c0831f25a4eab023f8f5b26a45c2444a
Ivan <72516268+Ivanbbarker@users.noreply.github.com>
2023-04-20 20:09:08 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -8,19 +8,3 @@
         modelo varchar(255),
         primary key (id)
     ) engine=InnoDB;
-
-    create table bicicleta (
-       id bigint not null auto_increment,
-        cliente varchar(255),
-        marca varchar(255),
-        modelo integer not null,
-        primary key (id)
-    ) engine=InnoDB;
-
-    create table bicicleta (
-       id bigint not null auto_increment,
-        cliente varchar(255),
-        marca varchar(255),
-        modelo varchar(255),
-        primary key (id)
-    ) engine=InnoDB;
