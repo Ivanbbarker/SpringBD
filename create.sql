156f186f53ea6098d7845d123f899dedd8cc4449
Ivan <ivancarreton@gmail.com>
2023-04-24 18:00:08 +0200

    l

--- create.sql
+++ create.sql
@@ -8,3 +8,27 @@
         modelo varchar(255),
         primary key (id)
     ) engine=InnoDB;
+
+    create table bicicleta (
+       id bigint not null auto_increment,
+        cliente varchar(255),
+        marca varchar(255),
+        modelo varchar(255),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table bicicleta (
+       id bigint not null auto_increment,
+        cliente varchar(255),
+        marca varchar(255),
+        modelo varchar(255),
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table bicicleta (
+       id bigint not null auto_increment,
+        cliente varchar(255),
+        marca varchar(255),
+        modelo varchar(255),
+        primary key (id)
+    ) engine=InnoDB;
